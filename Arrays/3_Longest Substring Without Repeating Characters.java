c731710b8edbec429bfde862213560144daf0009
Abhishek Yadav <abhasnp55@gmail.com>
2025-12-30 21:58:46 +0530

    Adding solution of leetcode Q3

---
+++ Arrays/3_Longest Substring Without Repeating Characters.java
@@ -0,0 +1,20 @@
+import java.util.HashSet;
+import java.util.Set;
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        Set<Character> charSet = new HashSet<>();
+        int maxLength = 0;
+        int left = 0;
+
+        for(int right =0; right<s.length(); right++){
+
+            while(charSet.contains(s.charAt(right))){
+                charSet.remove(s.charAt(left));
+                left++;
+            }
+            charSet.add(s.charAt(right));
+            maxLength = Math.max(maxLength, right - left +1);
+        }
+        return maxLength;
+    }
+}

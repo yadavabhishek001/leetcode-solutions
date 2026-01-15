a9d33f17c6155f13873c580b18e1bc73cbc2e9c3
leetcode-solutions <abhasnp55@gmail.com>
2026-01-15 23:35:57 +0530

    Adding solution of some mistakes

--- Arrays/203_Remove_Linked_List_Elements.java
+++ Arrays/203_Remove_Linked_List_Elements.java
@@ -1,4 +1,12 @@
+import java.util.*;
 class Solution {
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
     public ListNode removeElements(ListNode head, int val) {
         while(head != null && head.val == val){
             head = head.next;

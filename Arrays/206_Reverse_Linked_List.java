eb473d9768f5f468b5a4598ebf859e2be879dba5
leetcode-solutions <abhasnp55@gmail.com>
2026-01-18 16:16:13 +0530

    Adding class ListNode

--- Arrays/206_Reverse_Linked_List.java
+++ Arrays/206_Reverse_Linked_List.java
@@ -1,4 +1,12 @@
+
 class Solution {
+    class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
     public ListNode reverseList(ListNode head) {
         ListNode prev = null;
         ListNode curr = head;

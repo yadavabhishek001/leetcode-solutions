4a62f2c1a4a89516d98ab02e2d3fa7da0349753c
leetcode-solutions <abhasnp55@gmail.com>
2026-01-19 19:28:57 +0530

    Adding solution of leetcode Q234

---
+++ Arrays/234_Palindrome_Linked_List.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode mid = slow;
+        ListNode prev = null;
+        ListNode curr = mid;
+        ListNode next;
+        while(curr != null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        ListNode right = prev;
+        ListNode left = head;
+
+        while(right != null){
+            if(left.val != right.val){
+                return false;
+            }
+            right = right.next;
+            left = left.next;
+        }
+        return true;
+    }
+}

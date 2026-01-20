904ea94f9bf46616dff851bacc2b2cde5d642e78
leetcode-solutions <abhasnp55@gmail.com>
2026-01-20 20:40:03 +0530

    Adding class of ListNode

--- Arrays/19_Remove_Nth_Node_From_End_of_List.java
+++ Arrays/19_Remove_Nth_Node_From_End_of_List.java
@@ -1,4 +1,11 @@
 class Solution {
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
     public ListNode removeNthFromEnd(ListNode head, int n) {
         int sz = 0;
         ListNode curr = head;

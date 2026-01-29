5411246e246bf789f9339b30dc44731f0b540391
leetcode-solutions <abhasnp55@gmail.com>
2026-01-29 20:39:38 +0530

    Added solution of leetcode Q1991

---
+++ Arrays/1991_Find_the_Middle_Index_in_Array.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int findMiddleIndex(int[] nums) {
+        int n = nums.length;
+        float sum = 0;
+        for(int i = 0; i<n ; i++){
+            sum += nums[i];
+        }
+        float sum1 = 0;
+        for(int i = 0; i<n ; i++){
+            float a = (sum - nums[i]) / 2;
+            if(  a == sum1 ){
+                return i;
+            }
+            else{
+                sum1 += nums[i];
+            }
+        }
+        return -1;
+    }
+}

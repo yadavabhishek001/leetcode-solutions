f3afeb61c97d7fa8e05ed491c0997f87bf66406c
Abhishek Yadav <abhasnp55@gmail.com>
2026-09-06 17:45:55 +0530

    Time: 9 ms (92.21%), Space: 46.6 MB (91.39%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -5,49 +5,40 @@ class Solution {
 
         if(m > n) return new ArrayList<>();
 
-        int freq1[] = new int[26];
-        int freq2[] = new int[26];
+        int freq[] = new int[26];
 
         for(int i = 0; i<m ; i++){
             char ch = p.charAt(i);
-            freq1[ch - 'a']++;
+            freq[ch - 'a']++;
         }
-
-        List<Integer> list = new ArrayList<>();
         int left = 0;
-        int count = 0;
+        int count = m;
+        List<Integer> list = new ArrayList<>();
 
-        StringBuilder sb = new StringBuilder();
+        for(int right = 0; right<n ; right++){
+            char ch = s.charAt(right);
 
-        for(int i = 0; i<m ; i++){
-            sb.append(s.charAt(i));
-            freq2[s.charAt(i) - 'a']++;
-        }
-        if(Arrays.equals(freq1,freq2)) list.add(0);
-        
-        for(int i = m ; i<n ; i++){
-            char removed = s.charAt(i - m);
-            freq2[removed - 'a']--;
-            char ch = s.charAt(i);
-            sb.append(ch);
-            freq2[ch - 'a']++;
-            if(Arrays.equals(freq1,freq2)) list.add(i - m + 1);
+            if(freq[ch - 'a'] > 0){
+                count--;
+            }
+
+            freq[ch - 'a']--;
+
+            if(right - left + 1 > m){
+                char removed = s.charAt(left);
+
+                if(freq[removed - 'a'] >= 0){
+                    count++;
+                }
+
+                freq[removed - 'a']++;
+                left++;
+            }
+            if(count == 0){
+                list.add(left);
+            }
+
         }
         return list;
     }
-
-    //     for(int right = 0; right < n ; right++){
-    //         char ch = s.charAt(right);
-
-    //         if(freq[ch] > 0){
-    //             count++;
-    //         }
-
-    //         freq[ch]--;
-
-    //         while(count == p.length()){
-
-    //         }
-    //     }
-    // }
 }
